af0758ea8b315c87253b2afcfc4786435332b6eb
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-01-17 19:27:42 +0330

    HotFix NullOrEmpty Guard
    + Add ValueComesFromEnum

---
+++ src/Core/Source/ValueComesFrom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCenter.Core
+{
+    public enum ValueComesFrom
+    {
+        Configurations = 1,
+        Arguments = 2
+    }
+}
