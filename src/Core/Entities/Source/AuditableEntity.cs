52cee4c6f2b1f3d5e22edf58a9a85ca6026f0962
Arsalan <arsalanfallahpour@outlook.com>
2021-01-26 23:55:25 +0330

    Unit testing and Code Commenting (#21)
    * Added Comments to 'AuditableEntity' interface
    
    * Enabled Nullable reference types
    
    * Update Comments of 'AuditableEntity' interface
    
    * HotFix Bad Modeling and Added Comments
     to 'BaseAuditableEntity' interface
    
    Co-authored-by: arsalan falahpour <arsalan.falahpour@outlook.com>

--- src/Core/Entities/Source/AuditableEntity.cs
+++ src/Core/Entities/Source/AuditableEntity.cs
@@ -1,13 +1,34 @@
 namespace DotNetCenter.Core.Entities
 {
     using System;
-    public interface AuditableEntity<TAppKey> where TAppKey :  IEquatable<TAppKey>
+    /// <summary>
+    ///  Auditable Entity Interface
+    /// </summary>
+    /// <typeparam name="TKey">The Entity key type</typeparam>
+    public interface AuditableEntity<TKey, TKeyCreator> 
+        where TKey :  IEquatable<TKey>
+        where TKeyCreator :  IEquatable<TKeyCreator>
     {
         #region Properties
-        public TAppKey Id { get;}
-        public TAppKey CreatedBy { get; set; }
+        /// <summary>
+        /// Entity Identity (ID)
+        /// </summary>
+        public TKey Id { get;}
+        /// <summary>
+        /// Entity Creator Identity (ID)
+        /// </summary>
+        public TKeyCreator CreatedBy { get; set; }
+        /// <summary>
+        /// Entity Created Utc DateTime
+        /// </summary>
         public DateTime CreatedUtc { get; set; }
-        public TAppKey LastModifiedBy { get; set; }
+        /// <summary>
+        /// Entity Last Modifier identity (ID)
+        /// </summary>
+        public TKeyCreator LastModifiedBy { get; set; }
+        /// <summary>
+        /// Entity Last Modified Utc DateTime
+        /// </summary>
         public DateTime? LastModifiedUtc { get; set; }
         #endregion
     }
--- src/Core/Entities/Source/BaseAuditableEntity.cs
+++ src/Core/Entities/Source/BaseAuditableEntity.cs
@@ -1,24 +1,61 @@
 ﻿namespace DotNetCenter.Core.Entities
 {
-    using System; 
-    public class BaseAuditableEntity<TEntity, TAppKey> : AuditableEntity<TAppKey> 
-        where TEntity : BaseAuditableEntity<TEntity, TAppKey>
-        where TAppKey : IEquatable<TAppKey>
+    using System;
+    /// <summary>
+    /// Base class for Auditable Entities
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the Entity itself as parameter</typeparam>
+    /// <typeparam name="TKey">The Entity key type</typeparam>
+    /// <typeparam name="TKeyCreator">The Entity creator key type</typeparam>
+    public class BaseAuditableEntity<TEntity, TKey, TKeyCreator> : AuditableEntity<TKey, TKeyCreator>
+        where TEntity : BaseAuditableEntity<TEntity, TKey, TKeyCreator>
+        where TKey : struct, IEquatable<TKey>
+        where TKeyCreator : struct, IEquatable<TKeyCreator>
     {
         #region Constructors
-        public BaseAuditableEntity()
-        { }
-        public BaseAuditableEntity(TAppKey id)
+        /// <summary>
+        /// Default constructor with the Entity identity (ID) and the Creator identity (ID) initialization
+        /// </summary>
+        /// <param name="id">The Entity identity</param>
+        /// <param name="creatorId">The Entity creator identity (ID)</param>
+        public BaseAuditableEntity(TKey id, TKeyCreator creatorId)
+        {
+            Id = id;
+            CreatedBy = creatorId;
+        }
+        /// <summary>
+        /// Default constructor with the Entity Identity (ID) initialization
+        /// </summary>
+        /// <param name="id">The Entity id</param>
+        public BaseAuditableEntity(TKey id)
             => Id = id;
         #endregion
         #region Properties
-        public TAppKey Id { get; private set; }
-        public TAppKey CreatedBy { get; set; }
+        /// <summary>
+        /// Entity identity (ID)
+        /// </summary>
+        public TKey Id { get; private set; }
+        /// <summary>
+        /// Entity Creator identity (ID)
+        /// </summary>
+        public TKeyCreator CreatedBy { get; set; }
+        /// <summary>
+        /// Entity Created Utc DateTime
+        /// </summary>
         public DateTime CreatedUtc { get; set; }
-        public TAppKey LastModifiedBy { get; set; }
+        /// <summary>
+        /// Entity Last Modifier identity (ID)
+        /// </summary>
+        public TKeyCreator LastModifiedBy { get; set; }
+        /// <summary>
+        /// Entity Last Modified Utc DateTime
+        /// </summary>
         public DateTime? LastModifiedUtc { get; set; }
         #endregion
         #region Fields
+        /// <summary>
+        /// The Entity identity (ID) property name
+        /// </summary>
         public const string ID = nameof(Id);
         #endregion
     }
