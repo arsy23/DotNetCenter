bbc59d2adc96bdc513b4f1d65a1c810a5c17f334
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-01-26 21:12:07 +0330

    Create 'Money' ValueObject test case

---
+++ src/Core/Entities/Tests/UnitTest/Source/BeseValueObject/BaseValueObjectTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNetCenter.Core.Entities.UnitTest
+{
+    public class BaseValueObjectTest
+    {
+        private const int Amount = 23000;
+
+        #region GetEqualityComponents;
+        #region SameValueObjectsMustBeEquals
+        [Fact]
+        public void SameValueObjectsMustBeEquals()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount);
+            Assert.True(firstMoneyResource.Equals(secondMoneyResource));
+        }
+        #endregion
+        #region SameValueObjectsMustNotEquals
+        [Fact]
+        public void DiffrentValueObjectsMustNotEquals()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount + 32000);
+            Assert.False(firstMoneyResource.Equals(secondMoneyResource));
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ src/Core/Entities/Tests/UnitTest/Source/BeseValueObject/MoneyValueObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetCenter.Core.Entities.UnitTest
+{
+    public class MoneyValueObject : BaseValueObject<MoneyValueObject>
+    {
+        public MoneyValueObject(long amount) 
+            => _amount = amount;
+
+        public long Amount => _amount;
+        private readonly long _amount;
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Amount;
+        }
+
+        protected override int GetHashCodeCore() 
+            => System.HashCode.Combine(Amount);
+    }
+}
