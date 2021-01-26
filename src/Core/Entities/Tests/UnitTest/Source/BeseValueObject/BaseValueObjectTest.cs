ee8eccd200c1397bcd478ba3b975aa5637461556
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-01-26 23:06:32 +0330

    Add 'Operators' Unit-Tests

--- src/Core/Entities/Tests/UnitTest/Source/BeseValueObject/BaseValueObjectTest.cs
+++ src/Core/Entities/Tests/UnitTest/Source/BeseValueObject/BaseValueObjectTest.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace DotNetCenter.Core.Entities.UnitTest
+﻿namespace DotNetCenter.Core.Entities.UnitTest
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
     public class BaseValueObjectTest
     {
         private const int Amount = 23000;
 
-        #region GetEqualityComponents;
-        #region SameValueObjectsMustBeEquals
+        #region Equals
+        #region SameValueObjectsMustBeEqualsWithEqualsMethod
         [Fact]
-        public void SameValueObjectsMustBeEquals()
+        public void SameValueObjectsMustBeEqualsWithEqualsMethod()
         {
             var moneyuilder = new MoneyValueObjectBuilder();
             var firstMoneyResource = moneyuilder.GetMoney(Amount);
@@ -22,15 +22,61 @@ namespace DotNetCenter.Core.Entities.UnitTest
             Assert.True(firstMoneyResource.Equals(secondMoneyResource));
         }
         #endregion
-        #region SameValueObjectsMustNotEquals
+        #region DiffrentValueObjectsMustBeNotEqualsWithEqualsMethod
         [Fact]
-        public void DiffrentValueObjectsMustNotEquals()
+        public void DiffrentValueObjectsMustBeNotEqualsWithEqualsMethod()
         {
             var moneyuilder = new MoneyValueObjectBuilder();
             var firstMoneyResource = moneyuilder.GetMoney(Amount);
             var secondMoneyResource = moneyuilder.GetMoney(Amount + 32000);
             Assert.False(firstMoneyResource.Equals(secondMoneyResource));
         }
+        #endregion
+        #endregion
+        #region Operators
+        #region == Operator
+        #region SameValueObjectsMustBeEqualWithObjectEqualityOperator
+        [Fact]
+        public void SameValueObjectsMustBeEqualWithObjectEqualityOperator()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount);
+            Assert.True(firstMoneyResource == secondMoneyResource);
+        }
+        #endregion
+        #region DifferentValueObjectsMustNotEqualWithObjectEqualityOperator
+        [Fact]
+        public void DifferentValueObjectsMustNotEqualWithObjectEqualityOperator()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount + 32000);
+            Assert.False(firstMoneyResource == secondMoneyResource);
+        }
+        #endregion
+        #endregion
+        #region != Operator
+        #region SameValueObjectsComparisonMustBeTrueWithObjectNotEqualityOperator
+        [Fact]
+        public void SameValueObjectsComparisonMustBeFalseWithObjectNotEqualityOperator()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount);
+            Assert.False(firstMoneyResource != secondMoneyResource);
+        }
+        #endregion
+        #region DifferentValueObjectsComparisonMustBeTrueWithObjectNotEqualityOperator
+        [Fact]
+        public void DifferentValueObjectsComparisonMustBeTrueWithObjectNotEqualityOperator()
+        {
+            var moneyuilder = new MoneyValueObjectBuilder();
+            var firstMoneyResource = moneyuilder.GetMoney(Amount);
+            var secondMoneyResource = moneyuilder.GetMoney(Amount + 32000);
+            Assert.True(firstMoneyResource != secondMoneyResource);
+        }
+        #endregion
         #endregion
         #endregion
     }
