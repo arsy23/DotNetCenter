fbb1ab96ce35e57eb057310054932fbde0198a8c
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-01-29 15:28:04 +0330

    Fix Commenting

--- src/Core/Entities/Source/BaseAuditableEntity.cs
+++ src/Core/Entities/Source/BaseAuditableEntity.cs
@@ -20,37 +20,49 @@
         /// <param name="creatorId">The Entity creator identity (ID)</param>
         public BaseAuditableEntity(TKey id, TKeyCreator creatorId)
         {
-            Id = id;
-            CreatedBy = creatorId;
+            _id = id;
+            _creatorId = creatorId;
         }
         #endregion
         #region Properties
         /// <summary>
-        /// The Entity identity (ID)
+        /// Entity Identity (ID)
         /// </summary>
-        public TKey Id { get; private set; }
-        /// <summary>
-        /// The Creator Identity (ID)
-        /// </summary>
-        public TKeyCreator CreatedBy { get; set; }
-        /// <summary>
-        /// The UTC-DateTime that Entity it was Created
-        /// /// </summary>
-        public DateTime CreatedUtc { get; set; }
+        public TKey Id => _id;
+        private readonly TKey _id;
+
         /// <summary>
         /// The UTC-DateTime that Entity it was Created
         /// </summary>
-        public TKeyCreator LastModifiedBy { get; set; }
+        public TKeyCreator CreatedBy => _creatorId;
+        private readonly TKeyCreator _creatorId;
+
         /// <summary>
-        /// The LastModified-DateTime by The Entity
+        /// Initial UTC Date-Time that Entity it was Created
+        ///</summary>
+        public DateTime CreatedUtc => _createdUtc;
+        private readonly DateTime _createdUtc;
+
+        /// <summary>
+        /// Entity Last Modifier identity (ID)
         /// </summary>
-        public DateTime? LastModifiedUtc { get; set; }
+        public TKeyCreator LastModifiedBy => _lastModifiedBy;
+        private readonly DateTime? _lastModifiedUtc;
+
+        /// <summary>
+        /// The Last UTC Date-Time that Entity it was modified
+        /// </summary>
+        public DateTime? LastModifiedUtc => _lastModifiedUtc;
+        private readonly TKeyCreator _lastModifiedBy;
         #endregion
         #region Fields
         /// <summary>
         /// The Name of Identity (ID) Property
         /// </summary>
         public const string ID = nameof(Id);
+        #endregion
+        #region Methods
+        public abstract void Set
         #endregion
     }
 }
