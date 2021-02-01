35c88d3970ba1e0051684ee00f88fc64cdacfee4
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-01 23:20:53 +0330

    Commented-Out

--- src/Core/Source/ValueComesFrom.cs
+++ src/Core/Source/ValueComesFrom.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace DotNetCenter.Core
 {
+    /// <summary>
+    /// Determine Orign that value comes from
+    /// </summary>
     public enum ValueComesFrom
     {
+        /// <summary>
+        /// The Value comes from Configurations
+        /// </summary>
         Configurations = 0,
+        /// <summary>
+        /// The Value comes from Arguments
+        /// </summary>
         Arguments = 1
     }
 }
