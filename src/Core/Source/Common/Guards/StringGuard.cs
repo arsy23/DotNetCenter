b810706104ca33449c706833659eae1992a739f1
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-01 23:54:17 +0330

    Fixed and Commented-Out Guards

---
+++ src/Core/Source/Common/Guards/StringGuard.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetCenter.Core.Guards
+{
+    using System;
+    /// <summary>
+    /// Common String Guards
+    /// </summary>
+    public class StringGuard
+    {
+        /// <summary>
+        /// Guard For Less-Equal Zero value
+        /// </summary>
+        /// <param name="value">The Guarded Value</param>
+        /// <param name="paramName">The Value parameter name</param>
+        public static void ForLessEqualZero(int value, string paramName)
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(paramName);
+        }
+        /// <summary>
+        /// Guard For Null Or Empty value
+        /// </summary>
+        /// <param name="value">The Guarded Value</param>
+        /// <param name="paramName">The Value parameter name</param>
+        public static void ForNullOrEmpty(string value, string paramName)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(paramName);
+        }
+    }
+}
