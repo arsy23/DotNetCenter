a11e8d7f48ee2aa1a3efb686913d27d9d09fe384
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-01 23:33:29 +0330

    CodeFixing and Commenting

--- src/Core/Source/ObjectStates.cs
+++ src/Core/Source/ObjectStates.cs
@@ -1,10 +1,45 @@
 ﻿namespace DotNetCenter.Core
 {
-    public enum ObjectStates
+    /// <summary>
+    /// Common Object States
+    /// </summary>
+    public enum CommonObjectStates
     {
+        /// <summary>
+        /// The Object is Unchanged
+        /// </summary>
         Unchanged = 0,
-        Added     = 1,
-        Modified  = 2,
-        Deleted   = 3
+        /// <summary>
+        /// The Object is in Constructed state
+        /// </summary>
+        Constructed = 1
+        /// <summary>
+        /// The Object is Initialized
+        /// </summary>
+        Initialized = 2,
+        /// <summary>
+        /// The Object is Created
+        /// </summary>
+        Created   = 3,
+        /// <summary>
+        /// The Object is Modified
+        /// </summary>
+        Modified =  4,
+        /// <summary>
+        /// The Object is Attached to something
+        /// </summary>
+        Attached = 5,
+        /// <summary>
+        /// The Object is Detached from something
+        /// </summary>
+        Detached = 6,
+        /// <summary>
+        /// The Object is in Destructed state
+        /// </summary>
+        Destructed = 7,
+        /// <summary>
+        /// The Object is Deleted
+        /// </summary>
+        Deleted = 8
     }
 }
