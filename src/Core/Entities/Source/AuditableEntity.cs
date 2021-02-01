d92f28fcd561f3258d2698f384efa67991c8cd39
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-01 22:47:56 +0330

    1-CodeFixing, 2-Commenting
    1-Extract Interface from AuditableEntity
    2-Fix CodeNaming and Wrong Things

--- src/Core/Entities/Source/AuditableEntity.cs
+++ src/Core/Entities/Source/AuditableEntity.cs
@@ -5,36 +5,14 @@ namespace DotNetCenter.Core.Entities
     ///  Auditable Entity Interface
     /// </summary>
     /// <typeparam name="TKey">The Entity key type</typeparam>
-    /// <typeparam name="TKeyCreator">The Entity creator key type</typeparam>
-    public interface AuditableEntity<TKey, TKeyCreator> 
+    /// <typeparam name="TKeyCreator">The Entity Creator Key-Type</typeparam>
+    /// <typeparam name="TKeyModifier">The Entity Modifier Key-Type</typeparam>
+    public interface AuditableEntity<TKey, TKeyCreator, TKeyModifier> 
+        : Entity<TKey>, ModifiableEntity<TKeyModifier>, CreatableEntity<TKeyCreator>
         where TKey :  IEquatable<TKey>
         where TKeyCreator :  IEquatable<TKeyCreator>
     {
         #region Properties
-        /// <summary>
-        /// Entity Identity (ID)
-        /// </summary>
-        public TKey Id { get; }
-
-        /// <summary>
-        /// The UTC-DateTime that Entity it was Created
-        /// </summary>
-        public TKeyCreator CreatedBy { get; }
-
-        /// <summary>
-        /// Initial UTC Date-Time that Entity it was Created
-        ///</summary>
-        public DateTime CreatedUtc { get; }
-
-        /// <summary>
-        /// Entity Last Modifier identity (ID)
-        /// </summary>
-        public TKeyCreator LastModifiedBy { get; }
-
-        /// <summary>
-        /// The Last UTC Date-Time that Entity it was modified
-        /// </summary>
-        public DateTime? LastModifiedUtc { get; }
         #endregion
     }
 }
---
+++ src/Core/Entities/Source/CreatableEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCenter.Core.Entities
+{
+    /// <summary>
+    /// Interface for Creatable Entity
+    /// </summary>
+    /// <typeparam name="TKeyCreator">The Entity Creator Key-Type</typeparam>
+    public interface CreatableEntity<TKeyCreator>
+    {
+        /// <summary>
+        /// Entity Creator identity (ID)
+        /// </summary>
+        public TKeyCreator CreatedBy { get; }
+
+        /// <summary>
+        /// Initial Date-Time that Entity it was Created
+        ///</summary>
+        public DateTime CreatedDateTime { get; }
+    }
+}
