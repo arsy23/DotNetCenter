e3f95b75cf1925c3599b852705761e13aade8fb3
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-04 10:05:26 +0330

    Tested 'StringGuard' class

--- src/Core/Source/Common/Guards/StringGuard.cs
+++ src/Core/Source/Common/Guards/StringGuard.cs
@@ -4,27 +4,31 @@
     /// <summary>
     /// Common String Guards
     /// </summary>
-    public class StringGuard
+    public static class StringGuard
     {
-        /// <summary>
-        /// Guard For Less-Equal Zero value
-        /// </summary>
-        /// <param name="value">The Guarded Value</param>
-        /// <param name="paramName">The Value parameter name</param>
-        public static void ForLessEqualZero(int value, string paramName)
-        {
-            if (value <= 0)
-                throw new ArgumentOutOfRangeException(paramName);
-        }
+        #region ForNullOrEmpty
         /// <summary>
         /// Guard For Null Or Empty value
         /// </summary>
         /// <param name="value">The Guarded Value</param>
         /// <param name="paramName">The Value parameter name</param>
-        public static void ForNullOrEmpty(string value, string paramName)
+        public static void ForNullOrEmpty(string? value, string paramName)
         {
             if (string.IsNullOrEmpty(value))
                 throw new ArgumentNullException(paramName);
         }
+        #endregion
+        #region ForNullOrWhiteSpace
+        /// <summary>
+        /// Guard For Null Or White-Space value
+        /// </summary>
+        /// <param name="value">The Guarded Value</param>
+        /// <param name="paramName">The Value parameter name</param>
+        public static void ForNullOrWhiteSpace(string? value, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(paramName);
+        }
+        #endregion
     }
 }
---
+++ src/Core/Tests/UnitTest/Source/Common/Guards/StringGuardTest.cs
@@ -0,0 +1,93 @@
+﻿namespace DotNetCenter.Core.UnitTest.Common.Guards
+{
+    using DotNetCenter.Core.Guards;
+    using System;
+    using Xunit;
+
+    public class StringGuardTest
+    {
+
+        #region ForNullOrEmpty
+        #region GuardForNullOrEmpty*MustThorw*ArgumentNullExceptionWhenTheStringIsEmpty
+        [Fact]
+        public void GuardForNullOrEmptyMustThorwArgumentNullExceptionWhenTheStringIsEmpty()
+        {
+            var @string = string.Empty;
+            Type expected = GetArgumentNullExceptionType();
+            Assert.Throws(expected, () =>
+            StringGuard.ForNullOrEmpty(@string, nameof(@string)));
+        }
+        #endregion
+        #region GuardForNullOrEmpty*MustThorw*ArgumentNullExceptionWhenTheStringIsNull
+        [Fact]
+        public void GuardForNullOrEmptyMustThorwArgumentNullExceptionWhenTheStringIsNull()
+        {
+            string? @string = null;
+            Type expected = GetArgumentNullExceptionType();
+            Assert.Throws(expected, () =>
+            StringGuard.ForNullOrEmpty(@string, nameof(@string)));
+        }
+        #endregion
+        #region GuardForNullOrEmpty*MustNotThorw*ArgumentNullExceptionWhenTheStringHasValue
+        [Fact]
+        public void GuardForNullOrEmptyMustNotThorwArgumentNullExceptionWhenTheStringHasValue()
+        {
+            var @string = "Is not null.";
+            var exception = Record.Exception(()
+                => StringGuard.ForNullOrEmpty(@string, nameof(@string)));
+            Assert.Null(exception);
+        }
+        #endregion
+        #endregion
+        #region ForNullOrWhiteSpace
+        #region GuardForNullOrEmpty*MustThorw*ArgumentNullExceptionWhenTheStringIsAWhiteSpace
+        [Fact]
+        public void GuardForNullOrWhiteSpaceMustThorwArgumentNullExceptionWhenTheStringIsAWhiteSpace()
+        {
+            var @string = " ";
+            Type expected = GetArgumentNullExceptionType();
+            Assert.Throws(expected, () =>
+            StringGuard.ForNullOrWhiteSpace(@string, nameof(@string)));
+        }
+        #endregion
+        #region GuardForNullOrEmpty*MustThorw*ArgumentNullExceptionWhenTheStringIsWhiteSpaces
+        [Fact]
+        public void GuardForNullOrWhiteSpaceMustThorwArgumentNullExceptionWhenTheStringIsWhiteSpaces()
+        {
+            var @string = "            ";
+            Type expected = GetArgumentNullExceptionType();
+            Assert.Throws(expected, () =>
+            StringGuard.ForNullOrWhiteSpace(@string, nameof(@string)));
+        }
+        #endregion
+        #region GuardForNullOrWhiteSpace*MustThorw*ArgumentNullExceptionWhenTheStringIsNull
+        [Fact]
+        public void GuardForNullOrWhiteSpaceMustThorwArgumentNullExceptionWhenTheStringIsNull()
+        {
+            string? @string = null;
+            Type expected = GetArgumentNullExceptionType();
+            Assert.Throws(expected, () =>
+            StringGuard.ForNullOrWhiteSpace(@string, nameof(@string)));
+        }
+        #endregion
+        #region GuardForNullOrWhiteSpace*MustNotThorw*ArgumentNullExceptionWhenTheStringHasValue
+        [Fact]
+        public void GuardForNullOrWhiteSpaceMustNotThorwArgumentNullExceptionWhenTheStringHasValue()
+        {
+            var @string = "Is not null.";
+            var exception = Record.Exception(()
+                => StringGuard.ForNullOrWhiteSpace(@string, nameof(@string)));
+            Assert.Null(exception);
+        }
+        #endregion
+        #endregion
+
+        private static Type GetArgumentNullExceptionType()
+            => new ArgumentNullException().GetType();
+
+        #region TestCase
+        public class EEntity
+        { }
+        #endregion
+    }
+}
