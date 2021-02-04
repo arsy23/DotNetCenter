2f74f81c9555bfe7593f22a1c8c827d22576ef54
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-04 19:55:01 +0330

    Created abstract 'BaseObject' class

---
+++ src/Core/Source/BaseObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCenter.Core
+{
+    /// <summary>
+    /// Abstract class for ay object in DotNetCenter framework
+    /// </summary>
+    public abstract class BaseObject : object
+    {
+        #region GetEqualityComponents
+        /// <summary>
+        /// Get Equality Components
+        /// </summary>
+        /// <returns>yield return class components. eg. properties, method results and ...</returns>
+        protected abstract IEnumerable<object> GetEqualityComponents();
+        #endregion
+        #region GetHashCodeCore
+        /// <summary>
+        /// Abastract class for implement custom functionality to provide Hash-Code  
+        /// </summary>
+        /// <returns>Hash-Code</returns>
+        protected abstract int GetHashCodeCore();
+        #endregion
+        #region GetHashCode
+        /// <summary>
+        /// Provide Hash-Code for the this object
+        /// </summary>
+        /// <returns>Hash-Code</returns>
+        public override int GetHashCode()
+         => GetHashCodeCore();
+        #endregion
+    }
+}
