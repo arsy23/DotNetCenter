b66c7dd44e0aab56d4abb4eba9df427a2cf0167f
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-04 10:00:07 +0330

    Fixed 'Result' class name

---
+++ src/Core/ErrorHandlers/Source/ResultContainer.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetCenter.Core.ErrorHandlers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    public class ResultContainer
+    {
+        public ResultContainer(bool succeeded, IEnumerable<string> errors)
+        {
+            Succeeded = succeeded;
+            Errors = errors.ToArray();
+        }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public static ResultContainer Success()
+            => new ResultContainer(true, new string[] { });
+        public static ResultContainer Failure(IEnumerable<string> errors)
+            => new ResultContainer(false, errors);
+    }
+}
