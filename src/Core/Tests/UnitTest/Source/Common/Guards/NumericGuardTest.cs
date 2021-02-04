76ef6a06ac4f0c093d447724b46e9d108905e941
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-04 10:04:26 +0330

    Tested 'NumericGuard' class

---
+++ src/Core/Tests/UnitTest/Source/Common/Guards/NumericGuardTest.cs
@@ -0,0 +1,47 @@
+﻿namespace DotNetCenter.Core.UnitTest.Common.Guards
+{
+    using DotNetCenter.Core.Common.Guards;
+    using System;
+    using Xunit;
+    public class NumericGuardTest
+    {
+
+        #region ForLessEqualZero
+        #region GuardForLessEqualZero*MustNotThrow*ArgumentOutOfRangeExceptionWhenTheNumberGreaterThanZero
+        [Fact]
+        public void GuardForLessEqualZeroMustNotThrowArgumentOutOfRangeExceptionWhenTheNumberGreaterThanZero()
+        {
+            byte number = 1;
+            var exception = Record.Exception(() => NumericGuard.ForLessEqualZero(number, nameof(number)));
+            Assert.Null(exception);
+        }
+        #endregion
+        #region GuardForLessEqualZero*MustThrow*ArgumentOutOfRangeExceptionWhenTheNumberLessThanZero
+        [Fact]
+        public void GuardForLessEqualZeroMustThrowArgumentOutOfRangeExceptionWhenTheNumberLessThanZero()
+        {
+            short number = -1;
+            Type expected = GetArgumentOutOfRangeExceptionType();
+            Assert.Throws(expected, () => NumericGuard.ForLessEqualZero(number, nameof(number)));
+        }
+        #endregion
+        #region GuardForLessEqualZero*MustThrow*ArgumentOutOfRangeExceptionWhenTheNumberEqualZero
+        [Fact]
+        public void GuardForLessEqualZeroMustThrowArgumentOutOfRangeExceptionWhenTheNumberEqualZero()
+        {
+            short number = 0;
+            Type expected = GetArgumentOutOfRangeExceptionType();
+            Assert.Throws(expected, () => NumericGuard.ForLessEqualZero(number, nameof(number)));
+
+        }
+        #endregion
+        private static Type GetArgumentOutOfRangeExceptionType()
+            => new ArgumentOutOfRangeException().GetType();
+        #endregion
+
+        #region TestCase
+        public class EEntity
+        { }
+        #endregion
+    }
+}
