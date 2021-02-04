292ec9d5436ed543d6a6308782c904d5ba5543dc
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-04 21:32:45 +0330

    Commenting

--- src/Core/Source/Enumeration.cs
+++ src/Core/Source/Enumeration.cs
@@ -4,9 +4,14 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-
-    public abstract class Enumeration : IComparable
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Enumeration 
+        : BaseObject
+        , IComparable
     {
+        #region Constructors
         protected Enumeration()
         { }
         protected Enumeration(int id, string name)
@@ -14,11 +19,34 @@
             Id = id;
             Name = name;
         }
+        #endregion
+        /// <summary>
+        /// 
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
         public int Id { get; private set; }
-        public override string ToString()
-            => Name;
-        public static IEnumerable<T> GetAll<T>() where T : Enumeration, new()
+
+        #region Methods
+        /// <summary>
+        /// Get Hash-Code for this object
+        /// </summary>
+        /// <returns>Hash-Code</returns>
+        public override int GetHashCode()
+            => Id.GetHashCode();
+        #endregion
+
+        #region StaticMethods
+        #region public static IEnumerable<T> GetAll<T>() 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAll<T>() 
+            where T : Enumeration, new()
         {
             var type = typeof(T);
             var fields = type
@@ -32,28 +60,43 @@
                     yield return locatedValue;
             }
         }
-        public override bool Equals(object obj)
-        {
-            var otherValue = obj as Enumeration;
-            if (otherValue == null)
-                return false;
-            var typeMatches = GetType().Equals(obj.GetType());
-            var valueMatches = Id.Equals(otherValue.Id);
-            return typeMatches && valueMatches;
-        }
-        public override int GetHashCode()
-            => Id.GetHashCode();
+        #endregion
 
+        #region public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <returns></returns>
         public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
         {
             var absoluteDifference = Math.Abs(firstValue.Id - secondValue.Id);
             return absoluteDifference;
         }
+        #endregion
+
+        #region public static T FromValue<T>(int value) where T : Enumeration, new()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static T FromValue<T>(int value) where T : Enumeration, new()
         {
             var matchingItem = Parse<T, int>(value, "value", item => item.Id == value);
             return matchingItem;
         }
+        #endregion
+
+        #region public static T FromDisplayName<T>(string displayName) where T : Enumeration, new()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
         public static T FromDisplayName<T>(string displayName) where T : Enumeration, new()
         {
             var matchingItem = Parse<T, string>(displayName,
@@ -61,6 +104,7 @@
                                                 item => item.Name == displayName);
             return matchingItem;
         }
+        #endregion
         private static T Parse<T, K>(K value,
                                      string description,
                                      Func<T, bool> predicate) where T : Enumeration, new()
@@ -73,7 +117,27 @@
             }
             return matchingItem;
         }
-        public int CompareTo(object other)
+
+        #endregion
+
+        #region Overrides
+        /// <summary>
+        /// Determine wheter the specified object is equal to the current object; otherwize,false
+        /// </summary>
+        /// <param name="obj">The Target Object</param>
+        /// <returns>true if the specified object is equal to the current object; otherwize,false</returns>
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+            if (otherValue == null)
+                return false;
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+            return typeMatches && valueMatches;
+        }
+
+        #endregion
+               public int CompareTo(object other)
             => Id.CompareTo(((Enumeration)other).Id);
     }
 }
