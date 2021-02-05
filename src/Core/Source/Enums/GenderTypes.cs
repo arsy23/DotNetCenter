9eff7f22a3417418d723bc785271d73c2720aca2
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-05 17:53:14 +0330

    Commented and Fixed Code on 'GenderTypes'

---
+++ src/Core/Source/Enums/GenderTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetCenter.Core.Enums.Gender
+{
+    /// <summary>
+    /// Gender Types Enumeration
+    /// </summary>
+    public enum GenderTypes
+    {
+        /// <summary>
+        /// Pronouns: She/Her
+        /// </summary>
+        Female = 0,
+        /// <summary>
+        /// Pronouns: He, Him
+        /// </summary>
+        Male = 1
+    }
+}
