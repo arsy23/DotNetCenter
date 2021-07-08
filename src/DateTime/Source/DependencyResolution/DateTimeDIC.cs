714b683fa4f2b1a1d85e4bef02449db47e5c4f39
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-07-08 19:28:51 +0430

    Create TryAddDefaultDateTime Service Injection

--- src/DateTime/Source/DependencyResolution/DateTimeDIC.cs
+++ src/DateTime/Source/DependencyResolution/DateTimeDIC.cs
@@ -2,6 +2,8 @@
 {
     using DotNetCenter.DateTime.Common;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+
     /// <summary>
     /// DateTime Dependency Injection Container
     /// </summary>
@@ -11,6 +13,12 @@
         {
             services.AddTransient<CompoundableDateTimeNow, CompoundDateTimeNowService>();
             services.AddTransient<CompoundableDateTime, CompoundDateTimeService>();
+            return services;
+        }
+        public static IServiceCollection TryAddDefaultDateTimeServices(this IServiceCollection services)
+        {
+            services.TryAddTransient<CompoundableDateTimeNow, CompoundDateTimeNowService>();
+            services.TryAddTransient<CompoundableDateTime, CompoundDateTimeService>();
             return services;
         }
     }
